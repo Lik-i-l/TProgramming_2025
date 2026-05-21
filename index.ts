540f0df15d1cf2246bda90304a30a4f7d91998f8
Lik-i-l <nik28092000@yandex.ru>
2026-05-21 18:08:30 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 const b: number = 2.5;
-const xValues: number[] = [1.28, 3.28, 0.4, 1.1, 2.4, 3.6, 1.7, 3.9];
+
+const taskA: number[] = [1.28, 3.28, 0.4, 1.1];
+const taskB: number[] = [2.4, 3.6, 1.7, 3.9];
 
 function f(x: number, b: number): number {
-    const inner: number = b ** 3 + x ** 3;
-    const numerator: number = 1 + Math.sin(inner) ** 2;
-    const denominator: number = Math.cbrt(inner);
-    return numerator / denominator;
+  const inner: number = b ** 3 + x ** 3;
+  const numerator: number = 1 + Math.sin(inner) ** 2;
+  const denominator: number = Math.cbrt(inner);
+  return numerator / denominator;
 }
 
-for (const x of xValues) {
-    const y: number = f(x, b);
-    console.log(`x = ${x}, y = ${y.toFixed(2)}`);
-}
+console.log("Задача A");
+taskA.forEach((x: number, index: number) => {
+  const y: number = f(x, b);
+  console.log(`x${index + 1} = ${x}, y = ${y.toFixed(2)}`);
+});
+
+console.log("Задача B");
+taskB.forEach((x: number, index: number) => {
+  const y: number = f(x, b);
+  console.log(`x${index + 1} = ${x}, y = ${y.toFixed(2)}`);
+});
